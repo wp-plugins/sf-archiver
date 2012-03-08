14194fced233a4eaa490d8d0a3d2f0c213496a19
GregLone <GregLone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-08 17:45:26 +0000

    SF Archiver 1.1 in tags
    git-svn-id: https://plugins.svn.wordpress.org/sf-archiver/tags/1.1@516373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/w3p-acpt-admin.inc.php
@@ -0,0 +1,345 @@
+<?php
+
+/* Settings for the administration page */
+add_action( 'admin_init', 'w3p_acpt_register_settings' );
+function w3p_acpt_register_settings() {
+	register_setting( 'w3p-acpt-settings', '_w3p_acpt', 'w3p_acpt_sanitize' );
+}
+
+
+/* When unistall the plugin */
+register_uninstall_hook( W3P_ACPT_FILE, 'w3p_acpt_uninstaller' );
+function w3p_acpt_uninstaller() {
+	delete_option( '_w3p_acpt' );
+}
+
+
+/* Menu item */
+add_action( 'admin_menu', 'w3p_acpt_menu' );
+function w3p_acpt_menu() {
+	add_submenu_page( 'options-general.php', W3P_ACPT_PLUGIN_NAME, W3P_ACPT_PLUGIN_NAME, 'manage_options', W3P_ACPT_PAGE_NAME, 'w3p_acpt_settings_page' );
+}
+
+
+/* Just adds a "Settings" link in the plugins list */
+add_filter( 'plugin_action_links', 'w3p_acpt_settings_action_links', 10, 2 );
+function w3p_acpt_settings_action_links( $links, $file ) {
+	if ( strstr( W3P_ACPT_FILE, $file ) != '' ) {
+		$settings_link = '<a href="' . admin_url( 'options-general.php?page='.W3P_ACPT_PAGE_NAME ) . '">' . __("Settings") . '</a>';
+		array_unshift( $links, $settings_link );
+	}
+	return $links;
+}
+
+
+/* Settings page */
+function w3p_acpt_settings_page() {
+	$acpt	= '_w3p_acpt';
+	$sets	= get_option($acpt);
+	$cpts	= get_post_types( array('show_ui' => true, '_builtin' => false) );							// Get the public custom post types
+	if ( isset($_GET['settings-updated']) )																// Flush rules only on settings save
+		flush_rewrite_rules();
+	?>
+<div class="wrap">
+	<div id="icon-<?php echo W3P_ACPT_PAGE_NAME; ?>" class="icon32" style="background: url(<?php echo W3P_ACPT_PLUGIN_URL; ?>/admin/icon32.png) 0 0 no-repeat"><br/></div>
+
+	<h2><?php echo esc_html( W3P_ACPT_PLUGIN_NAME ); ?></h2>
+
+	<p><?php _e("Here you can choose to enable the archive capability for the public custom post types.", W3P_ACPT_DOMAIN); ?></p>
+
+	<?php
+	if ( !count($cpts) )
+		echo '<p><i>'.__("Wait, where are my custom post types?! Why did you installed me if you don&#8217;t have custom post types? ;)", W3P_ACPT_DOMAIN).'</i></p>';
+	else { ?>
+	<form name="w3p_acpt" method="post" action="options.php" id="w3p_acpt">
+		<?php settings_fields( 'w3p-acpt-settings' ); ?>
+		<table class="form-table">
+		<?php
+		global $wp_post_types;
+		$no_icon = '<span style="display:inline-block;width:16px;height:16px;background:url('.admin_url('images/menu.png').') -276px -8px no-repeat;">&#160;</span>';
+		foreach ( $cpts as $cpt ) { ?>
+			<tr valign="top">
+				<th scope="row">
+					<?php
+					$icon = isset($wp_post_types[$cpt]->menu_icon) && $wp_post_types[$cpt]->menu_icon ? '<img alt="&#8226; " src="'.esc_url($wp_post_types[$cpt]->menu_icon).'"/>' : $no_icon;
+					echo $icon.' '.$wp_post_types[$cpt]->label; ?>
+				</th>
+				<td>
+					<?php
+					$ha_def = $wp_post_types[$cpt]->has_archive && (!isset($sets[$cpt]['enabled']) || (int) $sets[$cpt]['enabled'] == 2);		// Has archive but not in settings : this CPT already has an archive page before the plugin install
+					if ( $ha_def ) {
+						echo '<input type="hidden" name="'.$acpt.'['.$cpt.'][enabled]" value="2"/>'
+							.'<span class="description">'.__("This <abbr title=\"Custom Post Type\">CPT</abbr> already has an archive page.", W3P_ACPT_DOMAIN).'</span><br/>';
+					} else {
+						echo '<label>'
+								.'<input type="checkbox" name="'.$acpt.'['.$cpt.'][enabled]" value="1"'.(isset($sets[$cpt]['enabled']) && $sets[$cpt]['enabled'] ? ' checked="checked"' : '' ).'/> '
+								.__("Enable archive page", W3P_ACPT_DOMAIN)
+							.'</label><br/>';
+					}
+					$p_slug		= isset($wp_post_types[$cpt]->rewrite['slug']) ? $wp_post_types[$cpt]->rewrite['slug'] : $cpt;
+					$a_slug		= isset($wp_post_types[$cpt]->has_archive) && is_string($wp_post_types[$cpt]->has_archive) ? $wp_post_types[$cpt]->has_archive : $p_slug;
+					// Display default value for the CPTs that already have an archive page before the plugin install
+					$disp_o		= !isset($sets[$cpt]['enabled']) || (int) $sets[$cpt]['enabled'] == 2;
+					$disp_o_a	= $disp_o && (!isset($sets[$cpt]['a_slug']) || !$sets[$cpt]['a_slug']);
+					$disp_o_p	= $disp_o && (!isset($sets[$cpt]['p_slug']) || !$sets[$cpt]['p_slug']);
+					// For all CPTs now
+					$disp_def_a	= $disp_o_a || !isset($sets[$cpt]['enabled']) || !$sets[$cpt]['enabled'];
+					$disp_def_p	= $disp_o_p || !isset($sets[$cpt]['enabled']) || !$sets[$cpt]['enabled'];
+					echo '<label>'
+							.'<input type="checkbox" name="'.$acpt.'['.$cpt.'][rss]" value="1"'.(isset($sets[$cpt]['rss']) && $sets[$cpt]['rss'] ? ' checked="checked"' : '' ).'/> '
+							.__("Add the RSS feed <code>&lt;link/&gt;</code> in head", W3P_ACPT_DOMAIN)
+						.'</label>'
+						.(isset($sets[$cpt]['enabled']) && $sets[$cpt]['enabled'] ? ' - <a target="_blank" href="'.get_post_type_archive_feed_link($cpt).'">'.__("Feed url", W3P_ACPT_DOMAIN).'</a>' : '').'<br/>';
+					echo '<label>'
+							.sprintf(__("%s per page:", W3P_ACPT_DOMAIN), $wp_post_types[$cpt]->label)
+							.' <input type="text" class="small-text" name="'.$acpt.'['.$cpt.'][ppp]" value="'.(isset($sets[$cpt]['ppp']) && $sets[$cpt]['ppp'] ? (int) $sets[$cpt]['ppp'] : '').'"/>'
+						.'</label><br/>';
+					echo '<label>'
+							.__("Custom archive slug:", W3P_ACPT_DOMAIN)
+							.' <input type="text" name="'.$acpt.'['.$cpt.'][a_slug]" value="'.(isset($sets[$cpt]['a_slug']) && $sets[$cpt]['a_slug'] ? strtolower(esc_attr($sets[$cpt]['a_slug'])) : '').'"/>'
+						.'</label> '
+						.($disp_def_a ? '<span class="description">('.__("default value:", W3P_ACPT_DOMAIN).' "'.$a_slug.'")</span>' : '')
+						.(isset($sets[$cpt]['enabled']) && $sets[$cpt]['enabled'] ? ' <a target="_blank" href="'.get_post_type_archive_link($cpt).'">'.__("Go to the archive page", W3P_ACPT_DOMAIN).'</a>' : '').'<br/>';
+					echo '<label>'
+							.__("Custom posts slug:", W3P_ACPT_DOMAIN)
+							.' <input type="text" name="'.$acpt.'['.$cpt.'][p_slug]" value="'.(isset($sets[$cpt]['p_slug']) && $sets[$cpt]['p_slug'] ? strtolower(esc_attr($sets[$cpt]['p_slug'])) : '').'"/>'
+						.'</label> '
+						.($disp_def_p ? '<span class="description">('.__("default value:", W3P_ACPT_DOMAIN).' "'.$p_slug.'")</span>' : '').'<br/>';
+					echo '<label>'
+							.'<input type="checkbox" name="'.$acpt.'['.$cpt.'][hp]" value="1"'.(isset($sets[$cpt]['hp']) && $sets[$cpt]['hp'] ? ' checked="checked"' : '' ).'/> '
+							.__("Add this post type to the posts page", W3P_ACPT_DOMAIN)
+						.'</label><br/>';
+					echo '<label>'
+							.'<input type="checkbox" name="'.$acpt.'['.$cpt.'][mainrss]" value="1"'.(isset($sets[$cpt]['mainrss']) && $sets[$cpt]['mainrss'] ? ' checked="checked"' : '' ).'/> '
+							.__("Add this post type to the main RSS feed", W3P_ACPT_DOMAIN)
+						.'</label><br/>';
+					?>
+				</td>
+			</tr>
+		<?php } ?>
+			<tr valign="top">
+				<th scope="row">
+					<input type="submit" name="submit" class="button-primary" value="<?php _e("Save Changes"); ?>" />
+				</th>
+				<td style="vertical-align: middle;">
+					<label><input type="checkbox" name="<?php echo $acpt; ?>[delete_options]" value="1" /> <?php _e("Panic room: delete all the plugin settings", W3P_ACPT_DOMAIN); ?></label>
+				</td>
+		</table>
+	</form>
+</div>
+<?php }
+}
+
+
+/* Sanitize options */
+function w3p_acpt_sanitize($options) {
+	if ( isset($options['delete_options']) && (int) $options['delete_options'] == 1 )
+		return;
+
+	$options = w3p_cleanup_array($options);
+	if (count($options)) {
+		global $wp_post_types;
+		foreach ($options as $cpt => $opt) {
+			if (!isset($wp_post_types[$cpt]))
+				unset($options[$cpt]);
+			else {
+				if (isset($opt['a_slug']) && $opt['a_slug'])
+					$options[$cpt]['a_slug'] = trim($options[$cpt]['a_slug'], '/');
+				if (isset($opt['p_slug']) && $opt['p_slug'])
+					$options[$cpt]['p_slug'] = trim($options[$cpt]['p_slug'], '/');
+			}
+		}
+	}
+
+	return $options;
+}
+
+
+/* Clean up an array by removing empty values */
+if ( !function_exists( 'w3p_cleanup_array' ) ) {
+	function w3p_cleanup_array($input, $keep0 = false) {
+		if (!is_array($input)) {
+			return trim($input);
+		}
+		$non_empty_items = array();
+		foreach ($input as $key => $value) {
+			if(($keep0 && $value != '') || (!$keep0 && $value)) {
+				$non_empty_items[$key] = w3p_cleanup_array($value, $keep0);
+				if (!count($non_empty_items[$key]))
+					unset($non_empty_items[$key]);
+			}
+		}
+		return $non_empty_items;
+	}
+}
+
+
+/* Contextual help for the Settings page */
+add_action( 'contextual_help', 'w3p_acpt_contextual_help',10,3);
+function w3p_acpt_contextual_help($help, $screen_id, $screen) {
+	if ($screen_id != 'settings_page_'.W3P_ACPT_PAGE_NAME)
+		return $help;
+
+	$helpArr = array();
+
+	$txt1 = '<p>'.__('Each post type must have the &#171;&#160;Archive capability&#160;&#187; if you want to link to a page listing them.', W3P_ACPT_DOMAIN).'</p>'.
+			'<p>'.__('To enable the archive page, tick the &#171;&#160;Enable archive page&#160;&#187; checkbox in front of your custom post type. If the custom post type already has this capability, the checkbox won&#8217; show up.', W3P_ACPT_DOMAIN).'</p>';
+	$helpArr[] = array(
+		'id'		=> 'archive-page',
+		'title'		=> __('Archive page', W3P_ACPT_DOMAIN),
+		'content'	=> $txt1
+	);
+
+	$txt2 = '<p>'.__('By enabling the &#171;&#160;Archive capability&#160;&#187;, a RSS feed is also created. If you want to add this feed to your site, tick the checkbox and a <code>&lt;link/&gt;</code> tag will be inserted in the head of your site. But first, you should look if it isn&#8217;t already there.', W3P_ACPT_DOMAIN).'</p>'.
+			'<p>'.sprintf(__("The plugin also gives you an url to this feed: just copy the &#171;&#160;Feed url&#160;&#187; link url. You can also get this url by using the appropriate WordPress function:<br/> %s.", W3P_ACPT_DOMAIN), "<code>&lt;?php echo get_post_type_archive_feed_link('my-custom-post-type'); ?&gt;</code>").'</p>';
+	$helpArr[] = array(
+		'id'		=> 'rss-feed',
+		'title'		=> __('RSS feed', W3P_ACPT_DOMAIN),
+		'content'	=> $txt2
+	);
+
+	$txt3 = '<p>'.sprintf(__('You can customize the number of posts per page for each custom post type. Otherwise, this number will be the same than what you specified for your normal posts: %d.', W3P_ACPT_DOMAIN), get_option('posts_per_page')).'</p>'.
+			'<p>'.__('Hint: -1 to display all of them in the same page.', W3P_ACPT_DOMAIN).'</p>';
+	$helpArr[] = array(
+		'id'		=> 'posts-per-page',
+		'title'		=> __('Posts per page', W3P_ACPT_DOMAIN),
+		'content'	=> $txt3
+	);
+
+	$txt4 = '<p>'.__('You can even specify a new slug for the archive page or the single pages.', W3P_ACPT_DOMAIN).'</p>'.
+			'<p>'.__("Hint: you can use sub-slugs like &#8217;sub-slug/slug&#8217;, &#8217;collections/summer/tee-shirts&#8217;...", W3P_ACPT_DOMAIN).'</p>'.
+			'<p>'.__("Don&#8217;t forget that the RSS url change at the same time.", W3P_ACPT_DOMAIN).'</p>';
+	$helpArr[] = array(
+		'id'		=> 'slugs',
+		'title'		=> __('Slugs', W3P_ACPT_DOMAIN),
+		'content'	=> $txt4
+	);
+
+	$txt5 = '<p>'.__('You can add your custom post types to your posts page, with your normal posts. Just tick the &#171;&#160;Add this post type to the posts page&#160;&#187; checkbox, the archive page is still available.', W3P_ACPT_DOMAIN).'</p>'.
+			'<p>'.__('You can do the same thing with the main RSS feed: you can add your custom post types to your normal posts in the main RSS feed by ticking the &#171;&#160;Add this post type to the main RSS feed&#160;&#187; checkbox.', W3P_ACPT_DOMAIN).'</p>';
+	$helpArr[] = array(
+		'id'		=> 'posts-page-and-feed',
+		'title'		=> __('Posts page and main RSS feed', W3P_ACPT_DOMAIN),
+		'content'	=> $txt5
+	);
+
+	$txt6 = '<p>'.sprintf(__('Go to Appearance -> %s, look for the box called &#171;&#160;Post types&#160;&#187;, add your custom post type to your menu :)', W3P_ACPT_DOMAIN), '<a href="'.admin_url('nav-menus.php').'">'.__('Menus').'</a>').'</p>';
+	$helpArr[] = array(
+		'id'		=> 'whats-next',
+		'title'		=> __('What&#8217;s next?', W3P_ACPT_DOMAIN),
+		'content'	=> $txt6
+	);
+
+	$credits = '<p>'.sprintf(__('This plugin was created by %1$s and reviewed by %2$s for a security check.', W3P_ACPT_DOMAIN), 'Grégory Viguier', "<a title='Boite A Web' target='_blank' href='http://www.boiteaweb.fr'>Julio Potier</a>").'</p>'.
+			   '<p>'.sprintf(__('Plugin icon by %s.', W3P_ACPT_DOMAIN), "<a title='Double-J designs' target='_blank' href='http://www.doublejdesign.co.uk/'>Double-J designs</a>").'</p>';
+	$helpArr[] = array(
+		'id'		=> 'credits',
+		'title'		=> __('Credits', W3P_ACPT_DOMAIN),
+		'content'	=> $credits
+	);
+
+	$helpSide = '<p><strong>'.__('For more information:').'</strong></p>'.
+				"<p><a title='Screenfeed' target='_blank' href='http://www.screenfeed.fr/archi/'>".__('My blog (french)', W3P_ACPT_DOMAIN).'</a></p>';
+
+	
+	foreach($helpArr as $helpItem) {
+		$screen->add_help_tab($helpItem);
+	}
+
+	$screen->set_help_sidebar( $helpSide );
+}
+
+
+/* Metabox for CPTs in Apperance -> Menus */
+add_action('admin_head-nav-menus.php', 'w3p_acpt_add_nav_menu_metabox');
+function w3p_acpt_add_nav_menu_metabox( $object ) {
+	$post_types = get_post_types( array( 'show_in_nav_menus' => true, 'has_archive' => true ), 'object' );
+	if ( count($post_types) )
+		add_meta_box( 'add-cpt-archive', __('Post types', W3P_ACPT_DOMAIN), 'w3p_acpt_nav_menu_metabox', 'nav-menus', 'side', 'default' );
+	return $object;
+}
+
+
+/* The metabox */
+function w3p_acpt_nav_menu_metabox() {
+	global $nav_menu_selected_id, $locale;
+	$post_types = get_post_types( array( 'show_in_nav_menus' => true, 'has_archive' => true ), 'object' );
+
+	foreach ( $post_types as &$post_type ) {
+		$post_type->db_id = 0;
+		$post_type->object = 'cpt-archive';
+		$post_type->object_id = $post_type->name;
+		$post_type->menu_item_parent = 0;
+		$post_type->type = $post_type->name;
+		$post_type->title = $post_type->labels->name;
+		$post_type->url = '';
+		$post_type->target = '';
+		$post_type->attr_title = '';
+		$post_type->classes = array();
+		$post_type->xfn = '';
+	}
+
+	$walker = new Walker_Nav_Menu_Checklist( array() );
+
+	?>
+	<div id="cpt-archive" class="cpt-archivediv">
+		<div id="cpt-archive-all" class="tabs-panel tabs-panel-view-all tabs-panel-active" style="height:auto;max-height:205px;border-style:solid;border-width:1px;overflow:auto;padding:.5em .9em;">
+			<ul id="ctp-archivechecklist" class="list:cpt-archive categorychecklist form-no-clear" style="margin:0;">
+				<?php
+				$checkbox_items = walk_nav_menu_tree( array_map('wp_setup_nav_menu_item', $post_types), 0, (object) array( 'walker' => $walker) );
+
+				if ( isset( $_REQUEST['cpt-archive-tab'] ) && $_REQUEST['cpt-archive-tab'] == 'all' && ! empty( $_REQUEST['selectall'] ) )
+					$checkbox_items = preg_replace('/(type=(.)checkbox(\2))/', '$1 checked=$2checked$2', $checkbox_items);
+				echo $checkbox_items;
+				?>
+			</ul>
+		</div><!-- /.tabs-panel -->
+	</div>
+
+	<p class="button-controls">
+		<span class="list-controls">
+			<a href="<?php echo esc_url(add_query_arg( array( 'selectall' => 1, 'cpt-archive-tab' => 'all' ) )); ?>#cpt-archive" class="select-all"><?php _e('Select All'); ?></a>
+		</span>
+
+		<span class="add-to-menu">
+			<img class="waiting" src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" alt="" />
+			<input type="submit"<?php disabled( $nav_menu_selected_id, 0 ); ?> class="button-secondary submit-add-to-menu" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-ctp-archive-menu-item" id="submit-cpt-archive" />
+		</span>
+	</p>
+	<?php
+}
+
+
+/* Modify the "type_label" + add the "Original" link url */
+add_filter( 'wp_setup_nav_menu_item', 'w3p_acpt_nav_menu_type_label' );
+function w3p_acpt_nav_menu_type_label( $menu_item ) {
+	if ( isset($menu_item->object) && $menu_item->object == 'cpt-archive' ) {
+		$menu_item->type_label = __( 'Archive' );
+		$menu_item->url = get_post_type_archive_link( $menu_item->type );		// Adding this url is useless since we can't add the link name
+	}
+	return $menu_item;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ w3p-archiver.php
@@ -0,0 +1,181 @@
+<?php
+/*
+Plugin Name: SF Archiver
+Plugin URI: http://www.screenfeed.fr/archi/
+Description: A simple way to display archive pages for your custom post types
+Version: 1.1
+Author: Grégory Viguier
+Author URI: http://www.screenfeed.fr/greg/
+License: GPLv3
+Require: WordPress 3.3+
+*/
+
+define( 'W3P_ACPT_PLUGIN_NAME',	'SF Archiver' );
+define( 'W3P_ACPT_PAGE_NAME',	'w3p_acpt_config' );
+define( 'W3P_ACPT_VERSION',		'1.1' );
+define( 'W3P_ACPT_DOMAIN',		'w3p-acpt' );
+define( 'W3P_ACPT_FILE',		__FILE__ );
+define( 'W3P_ACPT_DIRNAME',		basename( dirname( W3P_ACPT_FILE ) ) );
+define( 'W3P_ACPT_PLUGIN_URL',	trailingslashit( WP_PLUGIN_URL ) . W3P_ACPT_DIRNAME );
+define( 'W3P_ACPT_PLUGIN_DIR',	trailingslashit( WP_PLUGIN_DIR ) . W3P_ACPT_DIRNAME );
+
+
+/* Language support */
+add_action( 'init', 'w3p_acpt_lang_init' );
+function w3p_acpt_lang_init() {
+	load_plugin_textdomain( W3P_ACPT_DOMAIN, false, W3P_ACPT_DIRNAME . '/languages/' );
+}
+
+
+/* Init */
+if (is_admin()) {
+
+	include_once(W3P_ACPT_PLUGIN_DIR.'/admin/w3p-acpt-admin.inc.php');								// Admin
+
+} else {
+
+	add_filter( 'wp_get_nav_menu_items', 'cpt_archive_menu_filter', 10, 3 );						// Alter the URL for cpt-archive objects
+	function cpt_archive_menu_filter( $items, $menu, $args ) {
+		foreach ( $items as &$item ) {
+			if ( $item->object != 'cpt-archive' )
+				continue;
+			$item->url = get_post_type_archive_link( $item->type );
+			if ( get_query_var( 'post_type' ) == $item->type ) {
+				$item->classes []= 'current-menu-item';
+				$item->current = true;
+			}
+		}
+		return $items;
+	}
+
+	add_action('pre_get_posts', 'w3p_acpt_ppp');													// Posts per page limit and CPTs on posts page
+	function w3p_acpt_ppp($q) {
+		if ( !$q->is_main_query() )
+			return;
+
+		if ( $q->is_home ) {																											// CPTs on posts page
+
+			global $wp_post_types;
+			$sets = get_option('_w3p_acpt');
+			$pts = array('post');
+			if ( is_array($sets) && count($sets) ) {
+				foreach ($sets as $type => $set) {
+					if (isset($set['hp']) && $set['hp'] && isset($wp_post_types[$type]))
+						$pts[] = $type;
+				}
+				$pts = apply_filters( 'w3p-acpt-types-in-posts-page', $pts);
+				$q->set('post_type', $pts);
+			}
+
+		} elseif ( isset($q->query_vars['post_type']) && $q->query_vars['post_type'] && $q->is_post_type_archive ) {					// Posts per page limit for archives
+
+			$pt = $q->query_vars['post_type'];
+			$sets = get_option('_w3p_acpt');
+	
+			if ( isset($sets[$pt]['enabled'], $sets[$pt]['ppp']) && $sets[$pt]['enabled'] && $sets[$pt]['ppp'] )
+				$q->query_vars['posts_per_page'] = $sets[$pt]['ppp'];
+
+		}
+	}
+
+	add_filter( 'request', 'w3p_acpt_feed' );														// Add Custom Post Types to the main feed
+	function w3p_acpt_feed( $qv ) {
+		if ( isset($qv['feed']) && !isset($qv['post_type']) ) {
+			global $wp_post_types;
+			$sets = get_option('_w3p_acpt');
+			$pts = array('post');
+			if ( is_array($sets) && count($sets) ) {
+				foreach ($sets as $type => $set) {
+					if (isset($set['mainrss']) && $set['mainrss'] && isset($wp_post_types[$type]))
+						$pts[] = $type;
+				}
+				$pts = apply_filters( 'w3p-acpt-types-in-main-feed', $pts);
+				$qv['post_type'] = $pts;
+			}
+		}
+		return $qv;
+	}
+
+	add_action('wp_head', 'w3p_acpt_feed_links');													// Add feed links in head
+	function w3p_acpt_feed_links() {
+		$sets = get_option('_w3p_acpt');
+		if ( !is_array($sets) || !count($sets) )
+			return;
+
+		global $wp_post_types;
+		$feed_type = feed_content_type();
+		foreach ( $sets as $pt => $set ) {
+			if ( isset($set['enabled'], $set['rss']) && $set['enabled'] && $set['rss'] && isset($wp_post_types[$pt]->label) ) {
+				$feed_title = apply_filters( 'post_type_feed_title', __( 'Subscribe to %s via RSS', W3P_ACPT_DOMAIN ), $pt );
+				echo '<link rel="alternate" type="'.$feed_type.'" title="'.sprintf( esc_html($feed_title), $wp_post_types[$pt]->label ).'" href="'.get_post_type_archive_feed_link($pt).'" />';
+			}
+		}
+	}
+
+}
+
+
+// Enable archive page and rewrite for CPT
+add_action('registered_post_type', 'w3p_acpt_archive_cpt', 10, 2);
+function w3p_acpt_archive_cpt($post_type, $args) {
+	global $wp_post_types, $wp_rewrite;
+	$sets = get_option('_w3p_acpt');
+
+	if ( !isset($sets[$post_type]['enabled']) || !$sets[$post_type]['enabled'] )
+		return;
+
+	if ( is_admin() || '' != get_option('permalink_structure') ) {									// Well, main of the code from here is just a copy/paste of a part of the register_post_type() function.
+
+		$args->has_archive	= is_string($args->has_archive) && strlen($args->has_archive) ? $args->has_archive : true;		// Force archive page
+		unset($args->rewrite['feeds']);																						// Force RSS feed
+
+		if ( ! is_array( $args->rewrite ) )
+			$args->rewrite = array();
+		if ( isset($sets[$post_type]['p_slug']) && $sets[$post_type]['p_slug'] )					// Check for settings first (post slug)
+			$args->rewrite['slug'] = strtolower(esc_attr($sets[$post_type]['p_slug']));
+		elseif ( empty( $args->rewrite['slug'] ) )
+			$args->rewrite['slug'] = $post_type;
+		if ( ! isset( $args->rewrite['with_front'] ) )
+			$args->rewrite['with_front'] = true;
+		if ( ! isset( $args->rewrite['pages'] ) )
+			$args->rewrite['pages'] = true;
+		if ( ! isset( $args->rewrite['feeds'] ) || ! $args->has_archive )
+			$args->rewrite['feeds'] = (bool) $args->has_archive;
+
+		if ( $args->hierarchical )
+			$wp_rewrite->add_rewrite_tag("%$post_type%", '(.+?)', $args->query_var ? "{$args->query_var}=" : "post_type=$post_type&name=");
+		else
+			$wp_rewrite->add_rewrite_tag("%$post_type%", '([^/]+)', $args->query_var ? "{$args->query_var}=" : "post_type=$post_type&name=");
+
+		if ( $args->has_archive ) {
+			if ( isset($sets[$post_type]['a_slug']) && $sets[$post_type]['a_slug'] ) {				// Check for settings first (archive slug)
+				$archive_slug = strtolower(esc_attr($sets[$post_type]['a_slug']));
+				$args->has_archive = $archive_slug;
+			} elseif ( $args->has_archive === true )
+				$archive_slug = $args->rewrite['slug'];
+			else
+				$archive_slug = $args->has_archive;
+//			$archive_slug = $args->has_archive === true ? $args->rewrite['slug'] : $args->has_archive;		// The original line
+			if ( $args->rewrite['with_front'] )
+				$archive_slug = substr( $wp_rewrite->front, 1 ) . $archive_slug;
+			else
+				$archive_slug = $wp_rewrite->root . $archive_slug;
+
+			$wp_rewrite->add_rule( "{$archive_slug}/?$", "index.php?post_type=$post_type", 'top' );
+			if ( $args->rewrite['feeds'] && $wp_rewrite->feeds ) {
+				$feeds = '(' . trim( implode( '|', $wp_rewrite->feeds ) ) . ')';
+				$wp_rewrite->add_rule( "{$archive_slug}/feed/$feeds/?$", "index.php?post_type=$post_type" . '&feed=$matches[1]', 'top' );
+				$wp_rewrite->add_rule( "{$archive_slug}/$feeds/?$", "index.php?post_type=$post_type" . '&feed=$matches[1]', 'top' );
+			}
+			if ( $args->rewrite['pages'] )
+				$wp_rewrite->add_rule( "{$archive_slug}/{$wp_rewrite->pagination_base}/([0-9]{1,})/?$", "index.php?post_type=$post_type" . '&paged=$matches[1]', 'top' );
+		}
+
+		$wp_rewrite->add_permastruct($post_type, "{$args->rewrite['slug']}/%$post_type%", $args->rewrite['with_front'], $args->permalink_epmask);
+
+		// Great, stop the copy/paste and just add the final touch. Easy, isn't it? ;)
+		$wp_post_types[$post_type] = $args;
+	}
+}
+
+?>
